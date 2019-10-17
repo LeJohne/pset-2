cf47228a1b885601169e9ab348969ac0ab98425e
John Goytia <JGoytia@ucvts.tec.nj.us>
2019-10-17 13:22:00 -0400

    Commit 3 - exercise 1 & 2

---
+++ src/area.js
@@ -0,0 +1,7 @@
+let l = 0;
+let w = 0;
+const a = l * 2.54;
+const b = w * 2.54
+let c = a * b;
+
+console.log("A(n) " + (l) + "-by-" + (w) + "-inch sheet of paper has an area of " + (c) + " square milimeter(s).");
---
+++ src/hello-user.js
@@ -0,0 +1,3 @@
+const readlineSync = require("readline-sync");
+const name =readlineSync.question("what is your name?");
+console.log("Hello, " + name + "!")
