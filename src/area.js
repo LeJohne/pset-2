48779627e3d09d23ff9327ec6a479a171993e6ce
John Goytia <JGoytia@ucvts.tec.nj.us>
2019-10-21 13:10:24 -0400

    exercise 3

--- src/area.js
+++ src/area.js
@@ -1,7 +1,11 @@
-let l = 0;
-let w = 0;
-const a = l * 2.54;
-const b = w * 2.54
-let c = a * b;
+const readlineSync = require("readline-sync");
+const l = readlineSync.question("Width: ");
+const w = readlineSync.question("Length: ");
 
-console.log("A(n) " + (l) + "-by-" + (w) + "-inch sheet of paper has an area of " + (c) + " square milimeter(s).");
+let a = l*2.54;
+let b = w*2.54;
+let c = a * b;
+let d = Math.round(c*100)/100;
+let e = d.toLocaleString();
+
+console.log("A(n) " + (l) + "-by-" + (w) + "-inch sheet of paper has an area of " + (e)  + " square milimeter(s).");
